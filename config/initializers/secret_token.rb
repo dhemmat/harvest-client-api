ecd11f4a007337bc382c7ba28507c34f02b6961f
dhemmat <dhemmat@gmail.com>
2014-10-15 00:05:02 -0400

    create rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HarvestClientApi::Application.config.secret_key_base = '265779bef0564b28b3c36a519ea6dcdd46d98869001828bbf6da6bb1cf840aa5766c371059de778ed3f41b42f892a50cf3b37e44edb02b978e98bd37873d96db'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HarvestClientApi::Application.config.session_store :cookie_store, key: '_harvest-client-api_session'
