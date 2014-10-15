dbf5fa85688b55d953473091e1c4aca81b124287
dhemmat <dhemmat@gmail.com>
2014-10-15 00:23:44 -0400

    added initial grape configuration

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 HarvestClientApi::Application.routes.draw do
+  mount HarvestClientAPI::APIv1 => '/v1'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
