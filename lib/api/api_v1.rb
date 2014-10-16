9687813278aaf5d50124cf495d1a6255732aacf6
dhemmat <dhemmat@gmail.com>
2014-10-16 00:39:34 -0400

    removed more unneeded code

--- lib/api/api_v1.rb
+++
@@ -1,7 +0,0 @@
-# module API
-# 	class ApiV1 < Grape::API
-# 	  Dir["#{Rails.root}/app/api/v1/*.rb"].each do |f|
-# 	    # mount "V1::#{File.basename(f, '.*').camelize}".constantize
-# 	  end
-# 	end
-# end
