63127afdfad9cafd55e5417fbebf4a33c5ea4ce2
dhemmat <dhemmat@gmail.com>
2014-11-21 00:57:04 -0400

    basic rabl integration for projects endpoint

--- app/api/v1/projects.rb
+++ app/api/v1/projects.rb
@@ -2,7 +2,6 @@ module API
   module V1
     class Projects < Grape::API
       version 'v1'
-      format :json
 
       before do
         @harvest = HarvestLoader.getHarvestInstance
@@ -10,13 +9,8 @@ module API
 
       resource :projects do
         desc "Return list of projects for client"
-        get do
-          projects = @harvest.projects.all(nil, :client => current_user.harvest_client_id)
-          projects_hash = Hash.new
-          projects_hash[:projects] = projects.map do |p|
-            p.slice("name","id").to_h
-          end
-          (projects_hash).to_json
+        get '', :rabl => "projects" do
+          @projects = @harvest.projects.all(nil, :client => current_user.harvest_client_id)
         end
       end
     end
--- config/application.rb
+++ config/application.rb
@@ -7,6 +7,10 @@ Bundler.require(*Rails.groups)
 module HarvestClientApi
   class Application < Rails::Application
     config.paths.add "app/api", glob: "**/*.rb"
-    config.autoload_paths += Dir["#{Rails.root}/app"]    
+    config.autoload_paths += Dir["#{Rails.root}/app"]
+    
+    config.middleware.use(Rack::Config) do |env|
+      env['api.tilt.root'] = Rails.root.join "app", "views", "api"
+    end    
   end
 end
