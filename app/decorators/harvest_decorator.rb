11b73e447f7a123d828135a9b71bba6b5411690e
dhemmat <dhemmat@gmail.com>
2014-11-24 23:38:10 -0400

    changes tabs for spaces

--- app/decorators/harvest_decorator.rb
+++ app/decorators/harvest_decorator.rb
@@ -2,17 +2,17 @@ class HarvestDecorator < Draper::Decorator
   delegate_all
 
   def tasks_by_client client
-  	projects = self.projects_by_client(client)
-  	tasks_by_projects(projects)
+    projects = self.projects_by_client(client)
+    tasks_by_projects(projects)
   end
 
   def tasks_by_projects projects
-  	unless projects.is_a? Array
-  		projects = [projects]
-  	end
+    unless projects.is_a? Array
+      projects = [projects]
+    end
 
-  	tasks_ids = tasks_ids_by_projects(projects)
-  	tasks_by_ids(tasks_ids)
+    tasks_ids = tasks_ids_by_projects(projects)
+    tasks_by_ids(tasks_ids)
   end
 
   def projects_by_client client
@@ -28,8 +28,8 @@ class HarvestDecorator < Draper::Decorator
   private
 
   def tasks_ids_by_projects projects
-  	tasks_ids = []
-  	projects.each do |project|
+    tasks_ids = []
+    projects.each do |project|
       task_assignments.all(project).each do |task_assignment|
         tasks_ids << task_assignment.task_id
       end
@@ -38,13 +38,13 @@ class HarvestDecorator < Draper::Decorator
   end
 
   def tasks_by_ids tasks_ids
-  	tasks.all.map do |task|
-	    if tasks_ids.include? (task["id"])
-	      task
-	    else
-	      nil
-	    end
-	  end.compact
+    tasks.all.map do |task|
+      if tasks_ids.include? (task["id"])
+        task
+      else
+        nil
+      end
+    end.compact
   end
 
 end
