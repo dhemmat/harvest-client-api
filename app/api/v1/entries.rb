66840130baec4292eb4f2f4b83ef11129f586604
dhemmat <dhemmat@gmail.com>
2014-12-18 07:17:49 -0400

    complete entries endpoints and refactor

--- app/api/v1/entries.rb
+++ app/api/v1/entries.rb
@@ -1,28 +1,18 @@
 module API
   module V1
     class Entries < Grape::API
+      helpers API::V1::Helpers::DateHelper
       version 'v1'
 
       before do
         @harvest =  HarvestDecorator.new(HarvestLoader.getHarvestInstance)
-        @start_date = Time.parse(params[:start_date] || "20140101")
-        @end_date = params[:end_date].nil? ? Time.now : Time.parse(params[:end_date])
       end
 
       resource :entries do
-        
         desc "entries for client"
         get '', :rabl => "v1/entries" do
-          @entries = @harvest.entries_by_client(current_user, @start_date, @end_date)
+          @entries = @harvest.entries_by_client(current_user, start_date, end_date)
         end
-
-        # desc "Return tasks for a particular project"
-        # get ':project_id/tasks' do
-        #   project = @harvest.projects.find(params[:project_id])
-        #   error!('Unauthorized', 401) unless @harvest.client_has_project?(current_user, project.id)
-        #   @tasks = @harvest.tasks_by_projects(project)
-        # end
-
       end
     end
   end
--- app/api/v1/projects.rb
+++ app/api/v1/projects.rb
@@ -1,6 +1,7 @@
 module API
   module V1
     class Projects < Grape::API
+      helpers API::V1::Helpers::DateHelper
       version 'v1'
 
       before do
@@ -14,11 +15,18 @@ module API
           @projects = @harvest.projects_by_client(current_user)
         end
 
-        desc "Return tasks for a particular project"
-        get ':project_id/tasks' do
+        desc "Return tasks for a project"
+        get ':project_id/tasks', :rabl => "v1/tasks" do
           project = @harvest.projects.find(params[:project_id])
           error!('Unauthorized', 401) unless @harvest.client_has_project?(current_user, project.id)
           @tasks = @harvest.tasks_by_projects(project)
+        end
+
+        desc "Returns entries for a project"
+        get ':project_id/entries', :rabl => "v1/entries" do
+          project = @harvest.projects.find(params[:project_id])
+          error!('Unauthorized', 401) unless @harvest.client_has_project?(current_user, project.id)
+          @entries = @harvest.entries_by_project(project, start_date, end_date)
         end
 
       end
